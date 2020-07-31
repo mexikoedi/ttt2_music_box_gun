4f9b47151d5cd1238eb4d32f15f82e02ea41c049
DennisWolfgang <31799620+DennisWolfgang@users.noreply.github.com>
2020-07-31 00:50:26 -0400

    Add files via upload

---
+++ ttt2_music_box_gun/lua/lang/spanish.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference( "spanish" )
+L[ "ttt2_music_box_gun_name" ] = "La Caja Musical"
+L[ "ttt2_music_box_gun_desc" ] = "¡Una fiesta detro de un arma! ¡Has que el mundo baile a tu ritmo!"
+L[ "ttt2_music_box_gun_help1" ] = "{primaryfire} Has música."
+L[ "ttt2_music_box_gun_help2" ] = "{secondaryfire} Reproduce el sonido de algún meme épico por 3 segundos."
