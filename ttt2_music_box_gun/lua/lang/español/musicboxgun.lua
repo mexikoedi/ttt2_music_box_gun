33b6110e34e2340b517a481f481f29f74b5a6850
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2020-07-31 14:38:08 +0200

    Fixed language name

--- ttt2_music_box_gun/lua/lang/español/musicboxgun.lua
+++ ttt2_music_box_gun/lua/lang/español/musicboxgun.lua
@@ -1,4 +1,4 @@
-L = LANG.GetLanguageTableReference( "spanish" )
+L = LANG.GetLanguageTableReference( "español" )
 L[ "ttt2_music_box_gun_name" ] = "La Caja Musical"
 L[ "ttt2_music_box_gun_desc" ] = "¡Una fiesta detro de un arma! ¡Has que el mundo baile al ritmo de la música!"
 L[ "ttt2_music_box_gun_help1" ] = "{primaryfire} Poner música."
