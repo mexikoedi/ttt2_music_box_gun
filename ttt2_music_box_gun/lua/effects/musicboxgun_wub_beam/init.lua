a890d7ce7107aab97b9aef76bbb69c7710c8eda2
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2020-10-01 19:09:19 +0200

    hotfix

--- ttt2_music_box_gun/lua/effects/musicboxgun_wub_beam/init.lua
+++ ttt2_music_box_gun/lua/effects/musicboxgun_wub_beam/init.lua
@@ -1,26 +1,24 @@
 if engine.ActiveGamemode( ) != "terrortown" then return end
-EFFECT.Mat = Material( "trails/laser" ) //Material( "effects/beam001_blu_dx80" ) effects/laser_tracer
-EFFECT.Mat2 = Material( "trails/plasma" ) //Material( "effects/beam001_blu" )
+EFFECT.Mat = Material( "trails/laser" )
+EFFECT.Mat2 = Material( "trails/plasma" )
 
-/*---------------------------------------------------------
-   Init( data table )
------------------------------------------------------------*/
 function EFFECT:Init( data )
     self.Position = data:GetStart( )
     self.WeaponEnt = data:GetEntity( )
+    if !IsValid( self.WeaponEnt ) then return end
+    if !IsValid( self.WeaponEnt:GetOwner( ) ) then return end
     self.WeaponEntO = self.WeaponEnt:GetOwner( )
     self.Attachment = data:GetAttachment( )
-    // Keep the start and end pos - we're going to interpolate between them
     self.StartPos = self:GetTracerShootPos( self.Position , self.WeaponEnt , self.Attachment )
     self.StartPosTemp = self.StartPos
     self.EndPos = data:GetOrigin( )
     self.Alpha = 255
 end
 
-/*---------------------------------------------------------
-   THINK
------------------------------------------------------------*/
 function EFFECT:Think( )
+    if !IsValid( self.WeaponEnt ) then return end
+    if !IsValid( self.WeaponEnt:GetOwner( ) ) then return end
+
     if self.Alpha then
         self.Alpha = self.Alpha - FrameTime( ) * 548
     end
@@ -33,22 +31,17 @@ function EFFECT:Think( )
         self.EndPos = Vector( )
     end
 
-    if self.Alpha == nil then return false end //newaly added
+    if self.Alpha == nil then return false end
     self:SetRenderBoundsWS( self.StartPos , self.EndPos )
     if ( self.Alpha < 0 ) then return false end
 
     return true
 end
 
-/*---------------------------------------------------------
-   Draw the effect
------------------------------------------------------------*/
 function EFFECT:Render( )
     if !IsValid( self.WeaponEnt ) then return end
     if ( self.Alpha < 1 ) then return end
-    local col = Color( 70 * math.sin( RealTime( ) * 3 ) + 180 , 120 * math.sin( RealTime( ) / 3 ) + 180 , 50 * math.cos( RealTime( ) + 3 ) + 180 ) //Color( 160, 160, 255 ) // 255 160, 255
-    //local col2 = Color( 220, 60, 190 )
-    //local col = Color( 60, 20, 220 )
+    local col = Color( 70 * math.sin( RealTime( ) * 3 ) + 180 , 120 * math.sin( RealTime( ) / 3 ) + 180 , 50 * math.cos( RealTime( ) + 3 ) + 180 )
     self.Length = ( self.StartPos - self.EndPos ):Length( )
     local texcoord = math.Rand( 0 , 1 )
     local width = 15
