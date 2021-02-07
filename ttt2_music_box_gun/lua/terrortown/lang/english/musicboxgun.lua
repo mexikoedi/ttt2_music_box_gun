d33b084c4aa2167d9191bdcfdb13e819fca446c6
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-02-07 14:35:28 +0100

    updated to new ttt2 language identifiers

--- ttt2_music_box_gun/lua/terrortown/lang/english/musicboxgun.lua
+++ ttt2_music_box_gun/lua/terrortown/lang/english/musicboxgun.lua
@@ -1,4 +1,4 @@
-L = LANG.GetLanguageTableReference( "english" )
+L = LANG.GetLanguageTableReference( "en" )
 L[ "ttt2_music_box_gun_name" ] = "Music Box Gun"
 L[ "ttt2_music_box_gun_desc" ] = "It's a party in a gun! Make the world dance to your beat!"
 L[ "ttt2_music_box_gun_help1" ] = "{primaryfire} Make music."
