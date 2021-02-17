5cf44038a3de5c4bee9be6251db7d5e3f4132bbd
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-02-17 22:12:53 +0100

    formatted code + updated folder name

---
+++ ttt2_music_box_gun/lua/terrortown/lang/en/musicboxgun.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference("en")
+L["ttt2_music_box_gun_name"] = "Music Box Gun"
+L["ttt2_music_box_gun_desc"] = "It's a party in a gun! Make the world dance to your beat!"
+L["ttt2_music_box_gun_help1"] = "{primaryfire} Make music."
+L["ttt2_music_box_gun_help2"] = "{secondaryfire} Play 3 seconds of a legendary meme sound."
