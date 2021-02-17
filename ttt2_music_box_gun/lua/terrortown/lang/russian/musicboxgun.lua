6f974a02034533c7f6cfc878e9fc4591ea63e431
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-02-17 22:13:16 +0100

    formatted code + updated folder name

--- ttt2_music_box_gun/lua/terrortown/lang/russian/musicboxgun.lua
+++
@@ -1,5 +0,0 @@
-L = LANG.GetLanguageTableReference( "ru" )
-L[ "ttt2_music_box_gun_name" ] = "Музыкальная шкатулка"
-L[ "ttt2_music_box_gun_desc" ] = "Это вечеринка в оружии! Заставь мир танцевать в своём ритме!"
-L[ "ttt2_music_box_gun_help1" ] = "{primaryfire} Создать музыку."
-L[ "ttt2_music_box_gun_help2" ] = "{secondaryfire} Воспроизвести 3 секунды легендарного мемного звука."
