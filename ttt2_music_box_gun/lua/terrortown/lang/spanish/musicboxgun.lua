0059963b8732e9f3ba6f81ad114866e3b4de9036
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-02-17 22:13:43 +0100

    formatted code + updated folder name

--- ttt2_music_box_gun/lua/terrortown/lang/spanish/musicboxgun.lua
+++
@@ -1,5 +0,0 @@
-L = LANG.GetLanguageTableReference( "es" )
-L[ "ttt2_music_box_gun_name" ] = "La Caja Musical"
-L[ "ttt2_music_box_gun_desc" ] = "¡Una fiesta detro de un arma! ¡Has que el mundo baile al ritmo de la música!"
-L[ "ttt2_music_box_gun_help1" ] = "{primaryfire} Poner música."
-L[ "ttt2_music_box_gun_help2" ] = "{secondaryfire} Reproduce el sonido de algún meme épico por 3 segundos."
